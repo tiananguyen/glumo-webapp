1c668f3715cbaa21650b1d5f8e7f27f26585fdb3
Jayati Patel <jayati984@gmail.com>
2019-03-02 16:09:40 -0800

    connect serialport

---
+++ serialport.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000, () => { //Start the server, listening on port 3000.
+    console.log("Listening to requests on port 3000...");
+})
+//Bind socket.io to our express server.
+var io = require('socket.io')(server);
+//Send index.html page on GET
+app.use(express.static('views'));
+
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+//Connect serial port to port.....
+const port = new SerialPort('');
+ //Read the line only when new line comes.
+const parser = port.pipe(new Readline({delimiter: '\r\n'}));
+ //Read data
+parser.on('data', (glucose) => {
+    console.log(glucose);
+    var today = new Date();
+    //emit the date i.e. {date, time, temp} to all the connected clients.
+    io.sockets.emit('glucose', {date: today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear(), time: (today.getHours())+":"+(today.getMinutes()), glucose:glucose});
+});
+//Show a log
+io.on('connection', (socket) => {
+    console.log("Connected.");
+})
