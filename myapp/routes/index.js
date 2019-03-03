0164b8c3d8cf2cc2580ce5cd5fd927456484d825
Jayati Patel <jayati984@gmail.com>
2019-03-03 01:54:04 -0800

    Committing to catch up to master

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,23 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-<<<<<<< HEAD
-/* GET new glucose */
-=======
 router.get('/records', function(req, res) {
   res.render('records.ejs');
 });
 
->>>>>>> 537c460df74207ecb3766fb8af6e1a7082f06f84
 router.get('/start', function(req, res) {
   res.render('start.ejs');
 });
 
-<<<<<<< HEAD
-=======
 router.get('/home', function(req, res) {
   res.render('index.ejs');
 });
 
->>>>>>> 537c460df74207ecb3766fb8af6e1a7082f06f84
 module.exports = router;
